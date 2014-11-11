12efe71c0173c8642015b00b6cb17edf99128921
anrosent <Anson_Rosenthal@brown.edu>
2014-11-10 22:24:02 -0500

    generate token on launch so we can deploy on app store

--- extension/background.js
+++ extension/background.js
@@ -1,3 +1,7 @@
+// Generate session token - all your input events will be tagged with this
+var MY_TOKEN = generateToken(2);
+
+
 /**
  * Possible parameters for request:
  *  action: "xhttp" for a cross-origin HTTP request
@@ -9,6 +13,9 @@
 chrome.runtime.onMessage.addListener(function(request, sender, callback) {
     console.log('sending AJAX');
     if (request.action == "xhttp") {
+        // Add your token to reported data
+        request.data.token = MY_TOKEN;
+
         var xhttp = new XMLHttpRequest();
         var method = request.method ? request.method.toUpperCase() : 'GET';
 
@@ -24,7 +31,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, callback) {
         if (method == 'POST') {
             xhttp.setRequestHeader('Content-Type', 'application/json');
         }
-        xhttp.send(request.data);
+        xhttp.send(JSON.stringify(request.data));
         return true; // prevents the callback from being called too early on return
     }
 });
+
+function generateToken(n){
+    var token = "";
+    for (var i = 0; i < n; i++){
+        token += Math.random().toString(36).replace(/[^a-z]+/g, '');
+    }
+    return token;
+}
